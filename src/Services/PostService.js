d9d59eb485739778a2cb252d8089501704edae4f
Sajithjay99 <sajithjay99@gmail.com>
2025-05-07 16:52:03 +0530

    Add methods for updating, deleting posts and retrieving image gallery

--- src/Services/PostService.js
+++ src/Services/PostService.js
@@ -31,3 +31,67 @@ class PostService {
       throw new Error("Failed to get posts");
     }
   }
+
+  async getPostById(postId) {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      const response = await axios.get(`${BASE_URL}/posts/${postId}`, config);
+      return response.data;
+    } catch (error) {
+      throw new Error("Failed to get post");
+    }
+  }
+
+  async updatePost(postId, postData) {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      const response = await axios.put(
+        `${BASE_URL}/posts/${postId}`,
+        postData,
+        config
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error("Failed to update post");
+    }
+  }
+
+  async deletePost(postId) {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      await axios.delete(`${BASE_URL}/posts/${postId}`, config);
+    } catch (error) {
+      throw new Error("Failed to delete post");
+    }
+  }
+
+  // New methods for Galleries and Videos
+  async getImageGallery() {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      const response = await axios.get(`${BASE_URL}/posts/media/images`, config);
+      return response.data;
+    } catch (error) {
+      throw new Error("Failed to get image gallery");
+    }
+  }
