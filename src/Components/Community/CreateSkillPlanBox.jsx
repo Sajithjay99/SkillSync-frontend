f5a23b3bb08f6d5a0cabf4251f89729f75b7695b
OshanWanasekara <oshanhavishkawanasekara@gmail.com>
2025-05-07 09:50:36 +0530

    Implemented CreateSkillPlanBox function

--- src/Components/Community/CreateSkillPlanBox.jsx
+++ src/Components/Community/CreateSkillPlanBox.jsx
@@ -4,3 +4,29 @@ import state from "../../Utils/Store";
 
 const CreateSkillPlanBox = () => {
   const snap = useSnapshot(state);
+
+  return (
+    <div
+      className="skill-plan-box"
+      onClick={() => {
+        state.createSkillPlanOpened = true;
+      }}
+    >
+      <div className="post_top">
+        <img
+          alt="Profile"
+          src={snap.currentUser?.image}
+          className="user-image"
+        />
+        <input
+          type="text"
+          placeholder={`Share your skill plan, ${snap.currentUser?.username}`}
+          className="skill-input"
+          readOnly
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CreateSkillPlanBox;g
