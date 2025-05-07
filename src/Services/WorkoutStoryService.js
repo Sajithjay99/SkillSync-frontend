86514932f4b271a139a7dbf7ddd2f71d52f0a255
Sajithjay99 <sajithjay99@gmail.com>
2025-05-07 17:04:43 +0530

    Add methods for retrieving, deleting, and updating workout stories in WorkoutStatusUpdateService

--- src/Services/WorkoutStoryService.js
+++ src/Services/WorkoutStoryService.js
@@ -38,3 +38,58 @@ class WorkoutStatusUpdateService {
       throw new Error("Failed to get workout stories");
     }
   }
+  async getAllWorkoutStories() {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      const response = await axios.get(
+        `${BASE_URL}/workoutStatusUpdates`,
+        config
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error("Failed to get all workout stories");
+    }
+  }
+
+  async deleteWorkoutStory(updateId) {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      await axios.delete(
+        `${BASE_URL}/workoutStatusUpdates/${updateId}`,
+        config
+      );
+    } catch (error) {
+      throw new Error("Failed to delete workout story");
+    }
+  }
+  async updateWorkoutStory(updateId, workoutStoryData) {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      const response = await axios.put(
+        `${BASE_URL}/workoutStatusUpdates/${updateId}`,
+        workoutStoryData,
+        config
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error("Failed to update workout story");
+    }
+  }
+}
+
+export default new WorkoutStatusUpdateService();
