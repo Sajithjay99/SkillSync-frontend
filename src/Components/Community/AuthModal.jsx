31a7b36197252bb36c28aeb474f1517a53ecce0a
Lasitha Ravimal <lasitharavimal7@gmail.com>
2025-05-07 08:59:05 +0530

    Add service imports and uploader initialization in AuthModal component

--- src/Components/Community/AuthModal.jsx
+++ src/Components/Community/AuthModal.jsx
@@ -14,3 +14,8 @@ import {
   GoogleOutlined,
   GithubOutlined,
 } from "@ant-design/icons";
+import UploadFileService from "../../Services/UploadFileService";
+import AuthService from "../../Services/AuthService";
+import UserService from "../../Services/UserService";
+
+const uploader = new UploadFileService();
