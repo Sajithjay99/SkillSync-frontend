1d7942b576258715765eb9f04d40d8dc0a16be54
Lasitha Ravimal <lasitharavimal7@gmail.com>
2025-05-07 08:44:25 +0530

    Add AuthModal component with initial structure and imports

---
+++ src/Components/Community/AuthModal.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import {
+  Modal,
+  Form,
+  Input,
+  Button,
+  Upload,
+  message,
+  Divider,
+  Space,
+} from "antd";
+import {
+  InboxOutlined,
+  GoogleOutlined,
+  GithubOutlined,
+} from "@ant-design/icons";
